5d4de1dafb0f065319ce24e231f2c84c52e3af8a
Matthew <matthewsamaha@gmail.com>
2025-04-26 23:26:35 -0400

    !!todo

--- src/components/sections/FeaturedImage.tsx
+++ src/components/sections/FeaturedImage.tsx
@@ -1,2 +1,3 @@
+// TODO: fix header text colours or background/transparency to make it readable and better
 // TODO: before creating featured-images section
     // 1. Finish resume content to include education + experience + leadership experience with mantineUI
